004104f699c5986997cdfd7342a8bca399498062
Jassim Saleh <jassim7256@gmail.com>
2021-09-16 23:54:18 +0100

    !boo Fixes THEGOLDENPRO/Halloween-Cog-goldy-bot#1

--- halloween.py
+++ halloween.py
@@ -1,10 +1,21 @@
 import nextcord
 from nextcord.ext import commands
 import asyncio
+import random
+import datetime
 
 from goldy_func import *
 from goldy_utility import *
-import config.msg as msg
+from config import msg as goldy_msg
+
+#Importing extenstion pack.
+import cogs.halloween_cog.msg as msg
+
+#Importing other extentions.
+from cogs.giphy import giphy
+from cogs.giphy_cog.api import gif
+
+from cogs.halloween_cog.candy import candy
 
 #Change 'your_cog' to the name you wish to call your cog. ('your_cog' is just a placeholder.)
 cog_name = "halloween"
@@ -13,14 +24,47 @@ class halloween(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-        client.load_extension(f'cogs.halloween_cog.candy')
-        client.load_extension(f'cogs.halloween_cog.shop')
-        print("🍬candy loaded! ")
+        #Unload cogs that halloween cog will override.
+        settings.ignore_cogs.append(["shop", "economy"])
 
+        #Loading halloween's extentions.
+        goldy.cogs.load(self.client, "cogs.halloween_cog.candy")
+        goldy.cogs.load(self.client, "cogs.halloween_cog.shop")
+
+    @commands.cooldown(1, 30, commands.BucketType.user)
     @commands.command(description="Has a 50% chance of being a jumpscare or giving you some candy.")
     async def boo(self, ctx):
         if await can_the_command_run(ctx, cog_name) == True:
+            random_num = random.randint(1, 7)
+
+            if random_num in [1, 2, 3]: #Send random scary image.
+                embed = nextcord.Embed(title="🎃Oh no, you've been spooked!", description="Opps better luck next time.", colour=settings.AKI_RED)
+                url = await gif.random(ctx, self.client, "scary game jumpscare", (0, 10))
+                embed.set_image(url=url)
+                embed.set_footer(text=msg.embed.footer_1)
+                await ctx.send(embed=embed)
+            
+            else: #Give some candy to the member.
+                amount = random.randint(2, 8)
+                await candy.member.add(ctx, self.client, amount)
+                
+                description_message = ((msg.embed.prize_context).format(ctx.author.mention, msg.candy_emoji, amount))
+                title_message = f"🍬 You Won Candy!"
+                embed = nextcord.Embed(title=title_message, description=description_message, colour=settings.AKI_RED)
+                embed.set_footer(text=msg.embed.footer_1)
+
+                embed.set_thumbnail(url=msg.boo.embed.gif_url)
+
+                await ctx.send(embed=embed)
+
             pass
+
+    @boo.error
+    async def command_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.author.send(goldy_msg.error.cooldown.format(datetime.timedelta(seconds=round(error.retry_after))))
+        else:
+            await goldy.log_error(ctx, self.client, error, "halloween.boo")
 
     @commands.command(aliases=["trick-or-treat", "trick"])
     async def treat(self, ctx):
@@ -28,7 +72,7 @@ class halloween(commands.Cog):
             pass
 
     @commands.command(description="Sends you a random spooky image.")
-    async def spooky (self, ctx):
+    async def spooky(self, ctx):
         if await can_the_command_run(ctx, cog_name) == True:
             pass
 
@@ -37,8 +81,8 @@ class halloween(commands.Cog):
         if await can_the_command_run(ctx, cog_name) == True:
             pass
 
-    @commands.command(aliases=["skele", "skull"])
-    async def skeletons(self, ctx):
+    @commands.command(aliases=["skele", "skull", "skeletons"])
+    async def skeleton(self, ctx):
         if await can_the_command_run(ctx, cog_name) == True:
             pass
 
@@ -51,6 +95,12 @@ class halloween(commands.Cog):
     async def cram(self, ctx):
         if await can_the_command_run(ctx, cog_name) == True:
             pass
+
+    class embed():
+        @staticmethod
+        async def create(description="Happy Halloween!"):
+            embed=nextcord.Embed(title="**__🎃Halloween (2021)__**", description=description, color=settings.AKI_ORANGE)
+            return embed
 
 def setup(client):
     client.add_cog(halloween(client))
